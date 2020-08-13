8e6e8bfbe8b1801b46e12161206a27544bbb3233
Land-dev <ryan@awkvi.com>
2020-08-13 11:05:19 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("My name is Ryan");
+
+        System.out.println("I like to mountain bike, play tennis, and run cross country");
+
+        System.out.println("I'm looking to major in engineering, and am really interested in aerospace engineering");
+
+        System.out.println("However, I'm open to computer engineering as well, which is why I'm taking AP Comp Sci");
+
+        System.out.println("If I can, I'd like to go to the east coast for college so I can escape from California and never come back");
+
+    }
+}
